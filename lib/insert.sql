751b66b1041902e2bc2a5aa7416567e7f57d575a
Matteo Piccini <gnappo.1@live.it>
2023-01-02 03:55:50 -0800

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr. Chocolate", 30, "M", "Brown", "Aggressive", 1),
+("Rowdy", 20, "F", "Brown", "Peaceful", 1),
+("Tabitha", 3, "M", "White", "Aggressive", 0),
+("Sergeant Brown", 30, "M", "Brown", "Peaceful", 1),
+("Grinch", 15, "F", "Black", "Aggressive", 1),
+("Melissa", 90, "F", "Brown", "Peaceful", 1),
+("Wendy", 30, "M", "Brown", "Aggressive", 0),
+(NULL, 45, "F", "Black", "Peaceful", 1);
